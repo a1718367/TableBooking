68832352d1f380f7fe6f0e370b8ae97959205a1e
PzOng <pangszee.ong@gmail.com>
2020-05-06 18:04:02 +0930

    sample data

--- db/tableData.js
+++ db/tableData.js
@@ -1,2 +1,9 @@
-var tableBooking = [];
+var tableBooking = [
+    {
+        name: "Ris Otto",
+        phoneNumber: "88889999",
+        email: "risotto@food.com",
+        uniqueId: "101-tester",
+    }
+];
 module.exports = tableBooking;
